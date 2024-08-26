7a324f5af87c54c2ecc22dc446c2dc5ab103e22b
musmanirfan <usmanmala356@gmail.com>
2024-08-27 04:32:45 +0500

    name change

--- src/app/hobbies.tsx
+++ src/app/hobbies.tsx
@@ -1,17 +1,12 @@
 type HobbiesType = {
     hobbies?: string[]
 }
-
-
 export default function Hobbies({hobbies}:HobbiesType){
     return(
-        
-
         <ul>
             {
                 hobbies?.map((hobby, i)=><li key={i}>{hobby}</li>)
             }
-
         </ul>
     )
 }
