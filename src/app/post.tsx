44f93fd1ef6bb3f37dddef508a5869bd59cd32be
musmanirfan <usmanmala356@gmail.com>
2024-08-27 04:22:43 +0500

    name change

--- src/app/post.tsx
+++ src/app/post.tsx
@@ -2,6 +2,7 @@
 import { useEffect, useState } from "react"
 import { Face6, Favorite, MoreVert } from "@mui/icons-material";
 
+// Type definition for a post
 type PostType = {
     postName: string;
     postImage: string | null;
@@ -9,9 +10,7 @@ type PostType = {
     postLikes: number;
 };
 
-
 export default function Post() {
-
     const [getPost, setGetPost] = useState<PostType[]>([]);
 
     useEffect(() => {
@@ -37,34 +36,29 @@ export default function Post() {
     };
 
     return (
-        <>
-            <div>
-                {
-                    getPost.length > 0 && getPost?.map((post, i) => (
-                        <div className="w-[100%] flex justify-center">
-                            <div key={i} className="w-[30%] rounded-xl px-5 flex flex-col m-[0] shadow-2xl pt-5 pb-10 my-3">
-                                <div className="flex justify-between items-center">
-                                    <div className="flex gap-[5px] items-center">
-                                        <p>{<Face6 fontSize="large" />}</p>
-                                        <div>
-                                            <p className="font-bold  p-0">{post?.postName}</p>
-                                            <p className="text-[10px] mt-[-5px] p-0">last online</p>
-                                        </div>
-                                    </div>
-                                    <div onClick={() => handleDeletePost(i)} className="cursor-pointer">{<MoreVert />}</div>
-                                </div>
-                                <p className="text-[12px]">{post?.postDescription}</p>
-                                {post?.postImage && <img src={post?.postImage} alt="Post Image" />}
-                                <div className="flex justify-between items-center mt-1">
-                                    <p>{post?.postLikes} Likes</p>
-                                    <p>{<Favorite />}</p>
+        <div>
+            {getPost.map((post, i) => (
+                <div key={i} className="w-full flex justify-center">
+                    <div className="w-1/3 rounded-xl px-5 flex flex-col shadow-2xl pt-5 pb-10 my-3">
+                        <div className="flex justify-between items-center">
+                            <div className="flex gap-2 items-center">
+                                <Face6 fontSize="large" />
+                                <div>
+                                    <p className="font-bold">{post.postName}</p>
+                                    <p className="text-xs mt-[-5px]">last online</p>
                                 </div>
                             </div>
+                            <MoreVert onClick={() => handleDeletePost(i)} className="cursor-pointer" />
                         </div>
-                    ))
-
-                }
-            </div >
-        </>
-    )
+                        <p className="text-sm">{post.postDescription}</p>
+                        {post.postImage && <img src={post.postImage} alt="Post Image" />}
+                        <div className="flex justify-between items-center mt-1">
+                            <p>{post.postLikes} Likes</p>
+                            <Favorite />
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </div>
+    );
 }
